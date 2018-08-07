e35fa366630e78c287f3942742b62f18075319a2
perms <permsk@mail.ru>
2018-08-07 17:57:16 +0500

    Initial commit

---
+++ Breadcrumbs.php
@@ -0,0 +1,51 @@
+<?php
+namespace ruphp\uikit;
+
+use Yii;
+use yii\helpers\Html;
+
+/**
+ * Class Breadcrumbs
+ * @package frontend\widgets
+ * @see http://schema.org/BreadcrumbList
+ */
+class Breadcrumbs extends \yii\widgets\Breadcrumbs
+{
+    public $options = ['class' => 'uk-breadcrumb', 'itemscope itemtype' => "http://schema.org/BreadcrumbList"];
+    public $encodeLabels = false;
+
+    protected $itemPosition = 0;
+
+    /**
+     * @return mixed
+     */
+    public function run()
+    {
+        $this->itemPosition = 0;
+        return parent::run();
+    }
+
+    /**
+     * @param array $link
+     * @param string $template
+     * @return string
+     */
+    protected function renderItem($link, $template)
+    {
+        $this->itemPosition++;
+
+        if (!array_key_exists('label', $link)) {
+            throw new InvalidConfigException('The "label" element is required for each link.');
+        }
+        $link['itemprop'] = 'item';
+        $link['label'] = Html::tag('span', $link['label'], ['itemprop' => "name"]);
+
+        if (isset($link['url'])) {
+            $link['template'] = "<li itemprop=\"itemListElement\" itemscope itemtype=\"http://schema.org/ListItem\">{link}<meta itemprop=\"position\" content=\"{$this->itemPosition}\" /></li>\n";
+        } else {
+            $link['template'] = "<li class=\"uk-active\" itemprop=\"itemListElement\" itemscope itemtype=\"http://schema.org/ListItem\">{link}<meta itemprop=\"position\" content=\"{$this->itemPosition}\" /></li>\n";
+        }
+
+        return parent::renderItem($link, $template);
+    }
+}
---
+++ README.md
@@ -0,0 +1,50 @@
+# yii2-uikit3 extensions for Yii2
+
+Widgets & assets for a lightweight and modular front-end framework [UiKit](http://getuikit.com/) 
+
+
+Installation of Yii2 Uikit3 Extension
+------------------------------------
+
+The preferred way to install this extension is through [composer](http://getcomposer.org/download/). Just run
+
+```
+php composer.phar require --prefer-dist ruphp/yii2-uikit3
+```
+or add
+
+```
+"ruphp/yii2-uikit3:*"
+```
+to the require section of your `composer.json` file.
+
+
+Usage exaple:
+-------------
+
+For example, including main UiKit css & js files in any view files
+
+```php
+\ruphp\uikit\UikitAsset::register($this);
+```
+Uikit Slideshow widget examples:
+ * render slideshow from webroot path with images
+```php
+<?= \ruphp\uikit\widgets\Slideshow::widget(['path' => '/images/somefolder/']) ?>
+```
+ * render slideshow with custom slides
+```php
+<?= \ruphp\uikit\widgets\Slideshow::widget(['slides' => [
+		'<div class="slide">Any content with some <a href="#">button</a> e.g.</div>'
+		'<div class="pam_pam_slide-two">Another content with some <a href="#">button</a> e.g.</div>'
+	]
+]); ?>
+```
+ * render slideshow with overlays 
+```php
+<?= \ruphp\uikit\widgets\Slideshow::widget(['items' => [
+		['img'=>'/link/to/img.jpg','overlay'=>'Any content with some <a href="#">button</a> e.g.'],
+		['img'=>'/link/to/img.jpg','overlay'=>'Any content with some <a href="#">button</a> e.g.'],
+	]
+]); ?>
+```

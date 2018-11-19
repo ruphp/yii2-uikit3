202fe8ab2b64844d939dbd0a43e293003bfe9574
Александр <dev@wmapps.ru>
2018-11-19 23:30:01 +0500

    работа с nav подменю

--- GridView.php
+++ GridView.php
@@ -7,6 +7,6 @@ class GridView extends \yii\grid\GridView
 
 	public $tableOptions = ['class' => 'uk-table uk-form uk-table-condensed uk-table-hover'];
 	public $summaryOptions = ['class'=>'uk-text-center']; 
-	public $pager =  ['class'=> 'ruwmapps\yii2_uikit3\widgets\LinkPager'];
+    public $pager =  ['class'=> 'ruwmapps\yii2_uikit3\widgets\LinkPager'];
 
 }
